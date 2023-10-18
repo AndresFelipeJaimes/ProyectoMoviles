0bfb36d3e67b115c80906d068b2566e5b6d6710f
AndresFelipeJaimes <felipe.jaimes786@gmail.com>
2023-10-18 10:02:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyectomoviles/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.proyectomoviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Button registerButton2 = findViewById(R.id.btnRegister2);
+
+        registerButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
